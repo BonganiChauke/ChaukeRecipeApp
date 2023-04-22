fdec65f8b70b6d25ea28e0d049336a3a58391393
Bongani Chauke <ST10061533@rcconnect.edu.za>
2023-04-22 08:41:56 +0200

    Create Recipe class to declare automatic properties(fields) and using the attributes of these fields in class to store user inputs

--- ChaukeRecipeApp/Recipe.cs
+++ ChaukeRecipeApp/Recipe.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChaukeRecipeApp
 {
-    internal class Program
+    internal class Recipe
     {
-        static void Main(string[] args)
-        {
-
-
-        }
     }
 }

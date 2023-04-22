d986657a9b3f4e1135d89069e41ae97a9c7c8aa1
Bongani Chauke <ST10061533@rcconnect.edu.za>
2023-04-22 17:19:23 +0200

    Declaring objects and variable for user inputs and to use them in the methods

--- ChaukeRecipeApp/RecipeMethod.cs
+++ ChaukeRecipeApp/RecipeMethod.cs
@@ -8,5 +8,21 @@ namespace ChaukeRecipeApp
 {
     internal class RecipeMethod
     {
+        //Declaring Object reference for Recipe Class to get the attributes
+        Recipe recipeClass = new Recipe();
+
+        //Declaring variables for user inputs 
+        static string ingredientName;
+        static string ingredientUnitMeasurement;
+        static int ingredientQuantity;
+        static string ingredientStepsDescription;
+
+        //Single Arrays to assign them to properties in the Recipe class
+        static string ingredientNameArray;
+        static string ingredientUnitMeasurementArray;
+        static int ingredientQuantityArray;
+        static string ingredientStepsDescriptionArray;
+
+
     }
 }

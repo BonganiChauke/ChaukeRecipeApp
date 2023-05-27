1acaa356f1098df7a6ed3c2e25c67d64096d8aa4
Bongani Chauke <ST10061533@rcconnect.edu.za>
2023-05-27 14:06:46 +0200

    Declared Properties fields and an constructor that will be used to store user inputs

--- ChaukeRecipeApp/Ingredients.cs
+++ ChaukeRecipeApp/Ingredients.cs
@@ -8,5 +8,69 @@ namespace ChaukeRecipeApp
 {
     internal class Ingredients
     {
+        //Declaring properties variables 
+        private string ingredientName;//field
+        private double ingredientQuantity;//field
+        private string ingredientUnitMeasurement;//field
+        private int ingredientCalories;//field
+        private string ingredientFoodGroup;//field
+        
+        //Automatic property for ingredient name
+
+        public string IngredientName
+        {
+            get;set;
+        }
+
+        //Automatic property for ingredient quantity
+
+        public double IngredientQuantity
+        {
+            get;set;
+        }
+
+        //Automatic property for ingredient unit measurement 
+
+        public string IngredientUnitMeasurement
+        {
+            get;set;
+        }
+
+        //Automatic property for number of calories
+
+        public int IngredientCalories
+        {
+            get;set;
+        }
+
+        //Automatic property for select food group
+
+        public string IngredientFoodGroup
+        {
+            get;set;
+        }
+
+        //Overloaded constructor that will pass the values 
+
+        public Ingredients(string ingredientName, double ingredientQuantity, string ingredientUnitMeasurement,int ingredientCalories, string ingredientFoodGroup)
+        {
+            //setting the values for automatic properties
+
+            IngredientName = ingredientName;
+            IngredientQuantity = ingredientQuantity;
+            IngredientUnitMeasurement = ingredientUnitMeasurement;
+            IngredientCalories = ingredientCalories;
+            IngredientFoodGroup = ingredientFoodGroup;
+        }
+
+        //Override the constructor
+
+        public override string ToString()
+        {
+            return $"Ingredient Name: {IngredientName} Ingredient Quantity: {IngredientQuantity} Ingredient Unit Measurement: {IngredientUnitMeasurement} Ingredient Calories: {IngredientCalories} Ingredient Food Group: {IngredientFoodGroup}";
+        }
+
+
+
     }
 }

2bb1cfde32acafc86f474d221a00be069618c265
Bongani Chauke <ST10061533@rcconnect.edu.za>
2023-07-01 12:28:11 +0200

    Recipe Class [Update] Declared variable that will store recipe name have a contructor that will store ingredient and step classes as a list variable

--- ChaukeRecipeAppGUI/Recipe.cs
+++ ChaukeRecipeAppGUI/Recipe.cs
@@ -8,5 +8,47 @@ namespace ChaukeRecipeAppGUI
 {
     internal class Recipe
     {
+        //Declaring Properties Variables as private for recipe
+
+        public static String recipeName; //field
+
+
+        //Declaring Properties Variables as private for recipe
+
+        //Declarinng a class Ingredient as a list
+        public List<Ingredients> IngredientsClass
+        {
+            get; set;
+        }
+
+        //Declaring a class Steps as a list
+        public List<Steps> StepsClass
+        {
+            get; set;
+        }
+
+        //Automatic Property for ingredientName
+
+        public string RecipeName
+        {
+            get; set;
+        }
+
+
+        public Recipe() { }
+        //Overloaded Constructor for setting values to the  attributes
+
+        public Recipe(string recipeName)
+        {
+            RecipeName = recipeName;
+            IngredientsClass = new List<Ingredients>();
+            StepsClass = new List<Steps>();
+        }
+
+        //Override constructor with the values 
+        public override string ToString()
+        {
+            return $"Recipe Name: {RecipeName}";
+        }
     }
 }

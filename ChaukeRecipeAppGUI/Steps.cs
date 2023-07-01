d85a04bb1039c25ee4536579d63cf06e2304411c
Bongani Chauke <ST10061533@rcconnect.edu.za>
2023-07-01 18:19:12 +0200

    Steps Class [Update] The class steps will a loaded constructor that will save steps description into a list

--- ChaukeRecipeAppGUI/Steps.cs
+++ ChaukeRecipeAppGUI/Steps.cs
@@ -8,5 +8,35 @@ namespace ChaukeRecipeAppGUI
 {
     internal class Steps
     {
+        //Declaring properties variables
+        private string ingredientStepsDescription;//field
+
+        //Automatic property for ingredientStepsDescription field
+        public string IngredientStepsDescription
+        {
+            get; set;
+        }
+
+        //Declaring Ingredient class as list
+        public List<Ingredients> IngredientsClass
+        {
+            get; set;
+        }
+
+        public Steps() { }
+
+
+        //Overloaded Constructor for setting values to the  attributes
+        public Steps(string ingredientStepsDescription)
+        {
+            IngredientStepsDescription = ingredientStepsDescription;
+            IngredientsClass = new List<Ingredients>();
+        }
+
+        //Override the constructor with the values
+        public override string ToString()
+        {
+            return $"Step Description: {IngredientStepsDescription}";
+        }
     }
 }
